77c7d5f004766f4dcfa7563428e9c35ee35645eb
Leo Lutz <leo@leolutz.com>
2012-12-08 00:05:21 -0700

    Switch connection test to the new method and remove extraneous code
    This also contains a sample file.

--- src/isdk.php
+++ src/isdk.php
@@ -68,17 +68,17 @@ public function cfgCon($name,$dbOn="on") {
 ###Connect using API credentials###
 public function stdCon($appName,$key,$dbOn="on",$type="i") {
   $this->debug = $dbOn;
-  include('conn.cfg.php');
+
   if(!empty($appName)) {
     if($type=="i") {
-      $this->client = new xmlrpc_client("https://".$appName.".infusionsoft.com/api/xmlrpc");
+      $this->client = new xmlrpc_client("https://$appName.infusionsoft.com/api/xmlrpc");
     } else if($type=="m") {
-      $this->client = new xmlrpc_client("https://".$appName.".mortgageprocrm.com/api/xmlrpc");
+      $this->client = new xmlrpc_client("https://$appName.mortgageprocrm.com/api/xmlrpc");
     } else {
-      throw new Exception ("Invalid configuration for name: \"" . $appName . "\"");
+      throw new Exception ("Invalid application type: \"$appName\"");
     }
   } else {
-    throw new Exception("Invalid configuration name: \"" . $appName . "\"");
+    throw new Exception("Invalid application name: \"$appName\"");
   }
 
   ###Return Raw PHP Types###
@@ -91,12 +91,8 @@ public function stdCon($appName,$key,$dbOn="on",$type="i") {
   $this->key = $key;
 
   ###Connection verification###
-  $result = $this->dsGetSetting('Contact', 'optiontypes');
-  if (strpos($result, 'InvalidKey') == 12) {
-    return FALSE;
-  } else { return TRUE; }
-
-  return TRUE;
+  $test_conn = $this->appEcho($this->test_string);
+  return ($test_conn == $this->test_string);
 }
 
 ###Connect and Obtain an API key from a vendor key###
